e27dbbd4bb4253dbcaeb76415ce2c001b976b770
scarro <sergioca22@gmail.com>
2016-03-03 02:55:35 +0100

    agregando a gh-pages

--- paint.js
+++ paint.js
@@ -1,35 +1,48 @@
 /* Asociamos función canvasApp a carga de página */
-window.addEventListener('load', canvasApp, false);	
+window.addEventListener('load', canvasApp, false);  
 
 function canvasApp(){  
     /* Inicializamos el canvas */
-	var theCanvas = document.getElementById('canvas');
-	var context = theCanvas.getContext('2d');
+    var theCanvas = document.getElementById('canvas');
+    var context = theCanvas.getContext('2d');
 
     /* Inicializamos el valor del color */
-	var colorChosen = document.getElementById("color_chosen");
+    var colorChosen = document.getElementById("color_chosen");
+    var sizeChosen = document.getElementById("size_chosen");
+    var size = sizeChosen.innerHTML;
 
     /* Tomamos los botones de colores por su id */
-	var redButton = document.getElementById("Red");
-	var greenButton = document.getElementById("Green");
-	var blueButton = document.getElementById("Blue");
-	var blackButton = document.getElementById("Black");
-	var whiteButton = document.getElementById("White");
+    var redButton = document.getElementById("Red");
+    var greenButton = document.getElementById("Green");
+    var blueButton = document.getElementById("Blue");
+    var blackButton = document.getElementById("Black");
+    var whiteButton = document.getElementById("White");
+    var sizeButton = document.getElementById("size");
+
     /* Asociamos función colorPressed a pulsación de botón */
     redButton.addEventListener('click', colorPressed, false);
     greenButton.addEventListener('click', colorPressed, false);
     blueButton.addEventListener('click', colorPressed, false);
     blackButton.addEventListener('click', colorPressed, false);
     whiteButton.addEventListener('click', colorPressed, false);
+    sizeButton.addEventListener('click', sizePressed, false);
 
     function colorPressed(e) {
-	  var color_button_selected = e.target;
-	  var color_id = color_button_selected.getAttribute('id');
-	  colorChosen.innerHTML = color_id;
+      var color_button_selected = e.target;
+      var color_id = color_button_selected.getAttribute('id');
+      colorChosen.innerHTML = color_id;
+    }
+
+    function sizePressed(e){
+        var size_selected = document.getElementsByTagName("input")[6].value;
+        if(Number(size_selected)&&size_selected>0){
+            sizeChosen.innerHTML = size_selected;
+            size = size_selected;
+        }
     }
 
     /* Botón de reseteo */
-	var resetButton = document.getElementById("reset_image");
+    var resetButton = document.getElementById("reset_image");
     resetButton.addEventListener('click', resetPressed, false);
 
     /* Asociamos función colorPressed a pulsación de botón */
@@ -41,34 +54,34 @@ function canvasApp(){
     // For the mouse_moved event handler.
     var begin_drawing = false;
 
- 	drawScreen();
+    drawScreen();
 
     function drawScreen() {
-	  theCanvas.addEventListener('mousedown', mouse_pressed_down, false);
-	  theCanvas.addEventListener('mousemove', mouse_moved, false);
-	  theCanvas.addEventListener('mouseup', mouse_released, false);
+      theCanvas.addEventListener('mousedown', mouse_pressed_down, false);
+      theCanvas.addEventListener('mousemove', mouse_moved, false);
+      theCanvas.addEventListener('mouseup', mouse_released, false);
     }
 
     function mouse_pressed_down(ev) {
-  	  begin_drawing = true;
-	  context.fillStyle = colorChosen.innerHTML;
+      begin_drawing = true;
+      context.fillStyle = colorChosen.innerHTML;
     }
 
     function mouse_released(ev) {
-	  begin_drawing = false;
+      begin_drawing = false;
     }
 
     function mouse_moved(ev) {
-	  var x, y;	
-	  // Get the mouse position in the canvas
-	  x = ev.pageX;
-	  y = ev.pageY;
+      var x, y; 
+      // Get the mouse position in the canvas
+      x = ev.pageX-ev.target.offsetLeft;
+      y = ev.pageY-ev.target.offsetTop;
 
-	  if (begin_drawing) {
-	    context.beginPath();
-	    context.arc(x, y, 7, (Math.PI/180)*0, (Math.PI/180)*360, false);
-	    context.fill();
+      if (begin_drawing) {
+        context.beginPath();
+        context.arc(x, y, size, (Math.PI/180)*0, (Math.PI/180)*360, false);
+        context.fill();
         context.closePath();
-	  }
+      }
     }
 }
